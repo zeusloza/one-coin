1a073d2b1e747b560647f04ab6b81047926ec8c7
zeusloza <zivlcom@icloud.com>
2025-05-08 01:50:58 -0600

    cloudflare init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
@@ -28,4 +30,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
